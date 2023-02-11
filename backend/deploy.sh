b5ea6c295a4219f8eb8dc1764036e9dccd0168e6
antipovs <antipovs@yandex-team.ru>
2023-02-11 16:40:24 +0100

    Add deploy frontend

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -9,4 +9,4 @@ curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store.jar ${NEXUS_REPO_
 sudo cp ./sausage-store.jar /home/jarservice/sausage-store.jar||true #"<...>||true" говорит, если команда обвалится — продолжай#Обновляем конфиг systemd с помощью рестарта
 sudo systemctl daemon-reload
 #Перезапускаем сервис сосисочной
-sudo systemctl restart sausage-store-backend
+sudo systemctl restart sausage-store-backend.service
