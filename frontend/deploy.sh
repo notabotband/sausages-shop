234953ee5a3805366aba4b56ae94526968200a25
antipovs <antipovs@yandex-team.ru>
2023-02-20 11:45:22 +0100

    Fix deploy

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -4,7 +4,7 @@ set -xe
 #Перезаливаем дескриптор сервиса на ВМ для деплоя
 sudo cp -rf sausage-store-frontend.service /etc/systemd/system/sausage-store-frontend.service
 sudo rm -f /home/front-user/sausage-store.tar.gz||true
-sudo rm -f /home/front-user/frontend
+sudo rm -f /home/front-user/frontend||true
 #Переносим артефакт в нужную папку
 curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store.tar.gz ${NEXUS_REPO_URL}/sausage-store-antipov-stanislav-frontend/sausage-store/${VERSION}/sausage-store-${VERSION}.tar.gz
 sudo cp ./sausage-store.tar.gz /home/front-user/sausage-store.tar.gz||true #"<...>||true" говорит, если команда обвалится — продолжай#Обновляем конфиг systemd с помощью рестарта
