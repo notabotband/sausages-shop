888834f9e97c4b5446234908a40622f8fa2beee9
antipovs <antipovs@yandex-team.ru>
2023-04-01 09:56:06 +0200

    Add flyway

--- backend/src/main/resources/db/migration/V003__create_indexes.sql
+++ backend/src/main/resources/db/migration/V003__create_indexes.sql
@@ -1,2 +1,2 @@
-CREATE INDEX idx_order_pr_id ON order_product(product_id) WHERE product_id=2;
-CREATE INDEX idx_order_id ON orders(id);
+CREATE INDEX order_product_order_id_product_id ON order_product (order_id, product_id);
+CREATE INDEX order_product_product_id ON order_product (product_id);
