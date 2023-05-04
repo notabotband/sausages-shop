d2bf7cb95436f40e62d275801dc9f3fadd879b46
antipovs <antipovs@yandex-team.ru>
2023-05-04 18:42:00 +0200

    ADD secret

--- backend/backend_deploy.sh
+++ backend/backend_deploy.sh
@@ -11,11 +11,12 @@ docker network create -d bridge sausage_network || true
 docker login gitlab.praktikum-services.ru:5050 -u $DOCKER_GITLAB_USER -p $DOCKER_GITLAB_TOCKEN
 docker pull gitlab.praktikum-services.ru:5050/std-013-59/sausage-store/sausage-backend:latest
 
-docker stop backend || true
-docker rm backend || true
+docker stop sausage-backend || true
+docker rm sausage-backend || true
 set -e
 docker run -d --name sausage-backend \
     --network=sausage_network \
     --restart always \
     --pull always \
+    -p 8080:80 \
     gitlab.praktikum-services.ru:5050/std-013-59/sausage-store/sausage-backend:latest
