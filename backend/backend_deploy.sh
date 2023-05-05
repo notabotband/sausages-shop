433e8c21a5507e0a46aac55d293c7d6a84ab799b
antipovs <antipovs@yandex-team.ru>
2023-05-05 16:51:01 +0200

    ADD secret

--- backend/backend_deploy.sh
+++ backend/backend_deploy.sh
@@ -7,16 +7,15 @@ SPRING_DATASOURCE_PASSWORD=${SPRING_DATASOURCE_PASSWORD}
 SPRING_DATA_MONGODB_URI=${SPRING_DATA_MONGODB_URI}
 SPRING_VAULT_TOKEN=${SPRING_VAULT_TOKEN}
 EOF
-pwd
-sudo docker login -u $CI_REGISTRY_USER -p $CI_JOB_TOKEN $CI_REGISTRY
-sudo docker network create -d bridge sausage_network || true
-sudo docker login gitlab.praktikum-services.ru:5050 -u $DOCKER_GITLAB_USER -p $DOCKER_GITLAB_TOCKEN
-sudo docker pull gitlab.praktikum-services.ru:5050/std-013-59/sausage-store/sausage-backend:latest
-sudo docker stop sausage-backend || true
-sudo docker rm sausage-backend || true
+
+docker network create -d bridge sausage_network || true
+docker login gitlab.praktikum-services.ru:5050 -u $DOCKER_GITLAB_USER -p $DOCKER_GITLAB_TOCKEN
+docker pull gitlab.praktikum-services.ru:5050/std-013-59/sausage-store/sausage-backend:latest
+docker stop sausage-backend || true
+docker rm sausage-backend || true
 
 set -e
-sudo docker run -d --name sausage-backend \
+docker run -d --name sausage-backend \
     --network=sausage_network \
     --restart always \
     --pull always \
