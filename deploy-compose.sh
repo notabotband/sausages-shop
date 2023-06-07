7ed167572f569118090c23d40785799d8194745c
antipovs <antipovs@yandex-team.ru>
2023-06-07 17:28:32 +0200

    Add balancing

--- deploy-compose.sh
+++ deploy-compose.sh
@@ -4,9 +4,4 @@ set -e
 
 sudo docker login -u ${REGISTRY_USER} -p ${REGISTRY_ACCESS_TOKEN} https://gitlab.praktikum-services.ru:5050/
 sudo docker-compose --env-file /home/${DEV_USER}/.env_file --project-directory /home/${DEV_USER}/ pull
-
-sudo cp -rf /home/${DEV_USER}/sausage-store.service /etc/systemd/system/sausage-store.service
-sudo systemctl daemon-reload
-sudo systemctl restart sausage-store
-
-sudo rm /home/${DEV_USER}/sausage-store.service
+sudo docker-compose --env-file /home/${DEV_USER}/.env_file up -d --scale backend=3
