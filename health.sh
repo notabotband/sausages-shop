405bce7ce53f9888c26ade43c14949cd89325a32
antipovs <antipovs@yandex-team.ru>
2023-06-11 12:44:25 +0200

    Fix

--- health.sh
+++ health.sh
@@ -5,5 +5,5 @@ set +e
 sudo docker login -u ${REGISTRY_USER} -p ${REGISTRY_ACCESS_TOKEN} https://gitlab.praktikum-services.ru:5050/
 
 if [ "$( sudo docker container inspect -f '{{.State.Health.Status}}' green )" == "healthy" ] && ["$( sudo docker container inspect -f '{{.State.Health.Status}}' blue )" == "healthy" ];
-then [ sudo docker-compose --name green --env-file /home/${DEV_USER}/.env_file --project-directory /home/${DEV_USER}/ pull ] && [ sudo docker-compose stop green ] && [ sudo docker-compose --env-file /home/${DEV_USER}/.env_file up -d --scale green=3 blue=3 ] && [ sudo docker-compose stop blue ]
+then [ sudo docker-compose --env-file /home/${DEV_USER}/.env_file --project-directory /home/${DEV_USER}/ pull ] && [ sudo docker-compose stop green ] && [ sudo docker-compose --env-file /home/${DEV_USER}/.env_file up -d --scale green=3 blue=3 ] && [ sudo docker-compose stop blue ]
 fi
